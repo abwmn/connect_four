89af5ecb71c31259b18b32f2b349ad18eb18b805
Andrew Bowman <andrewtbowman@gmail.com>
2023-02-18 14:06:46 -0500

    feat: update board to link nodes

--- lib/board.rb
+++ lib/board.rb
@@ -1,10 +1,30 @@
 class Board
-  attr_reader :board
+  attr_reader :grid
 
   def initialize
-    @board = {}
+    @grid = {}
     (0..6).each do |col|
-      @board[col] = Array.new(6) { |row| Node.new(col, row, self) }
+      @grid[col] = Array.new(6) { |row| Node.new(col, row) }
+    end
+    link_nodes
+  end
+
+  def get_node(col, row)
+    @grid[col][row] if @grid[col] && @grid[col][row]
+  end
+  
+  def link_nodes
+    (0..6).each do |col|
+      (0..5).each do |row|
+        node = get_node(col, row)
+        node.ne = get_node(col+1, row+1)
+        node.e  = get_node(col+1, row)
+        node.se = get_node(col+1, row-1)
+        node.s  = get_node(col, row-1)
+        node.sw = get_node(col-1, row-1)
+        node.w  = get_node(col-1, row)
+        node.nw = get_node(col-1, row+1)
+      end
     end
   end
 end
