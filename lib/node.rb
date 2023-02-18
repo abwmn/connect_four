d6bcad9465a50b208965e54e44e87c5f9e37e1cd
Logan Cole <emtlogancole@gmail.com>
2023-02-18 12:51:46 -0600

    refactor: node, ground up

--- lib/node.rb
+++ lib/node.rb
@@ -1,37 +1,14 @@
 class Node
-  attr_reader :letter, :empty, :ne, :e, :se, :s, :sw, :w, :nw
-              
-  def initialize(col, row, board)
+  attr_reader :letter, :empty, :ne, :e, :se, :s, :nw, :w, :sw
+  def initialize
     @letter = '.'
     @empty = (@letter == '.')
-    @ne = board[col+1][row+1]
-    @e  = board[col+1][row]
-    @se = board[col+1][row-1]
-    @s  = board[col][row-1]
-    @sw = board[col-1][row-1]
-    @w  = board[col-1][row]
-    @nw = board[col-1][row+1]
-    @compass = { ne: sw, e: w, se: nw, s: nil }
-  end
-
-  def count(direction, letter, count = 0)
-    node = self
-    return count unless node&.direction&.letter == letter
-    node.direction.count(direction, letter, count + 1) 
-  end
-
-  def connect(letter = @letter)
-    connex = 0
-    @compass.each do |forth, back|
-      to = count(forth, letter)
-      fro = count(back, letter)
-      total = to + fro + 1
-      connex = [total, connex].max
-    end
-    connex
-  end
-
-  def connect?(length = 4, letter = @letter)
-    connect(letter) >= length
+    @ne = ne
+    @e = e
+    @se = se
+    @s = s
+    @nw = nw
+    @w = w
+    @sw = sw
   end
 end
