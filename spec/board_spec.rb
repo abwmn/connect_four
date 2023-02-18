9811690e6ecfe223f71bbe7ad0895f4e94ee27e1
Andrew Bowman <andrewtbowman@gmail.com>
2023-02-18 13:58:22 -0500

    test: add board spec

---
+++ spec/board_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+RSpec.describe Board do
+  before(:each) do 
+    @board = Board.new
+  end
+
+  it 'exists' do
+    expect(@board).to be_a(Board)
+  end
+
+  it 'has a grid hash' do  
+    expect(@board.grid).to be_a(Hash)
+  end
+
+  it 'is full of nodes' do
+    expect(@board.grid[0][0]).to be_a(Node)
+  end
+end
