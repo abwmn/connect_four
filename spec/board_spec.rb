a91076c68c4334da8ebe87bbcaed3936a428aaff
Logan Cole <emtlogancole@gmail.com>
2023-02-18 13:49:20 -0600

    test: board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -16,4 +16,9 @@ RSpec.describe Board do
   it 'is full of nodes' do
     expect(@board.grid[0][0]).to be_a(Node)
   end
+
+  it 'links nodes' do
+    expect(@board.grid[0][0].ne).to eq(@board.grid[1][1])
+    expect(@board.grid[3][5].sw).to eq(@board.grid[2][4])
+  end
 end
