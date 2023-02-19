6b3dc2a074a2634d0390832756226a367a888d6f
Andrew Bowman <andrewtbowman@gmail.com>
2023-02-18 21:48:40 -0500

    test: test counting methods

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,40 +1,34 @@
-require './spec/spec_helper'
+require './spec/spec_helper.rb'
 
 RSpec.describe do
   before(:each) do
-
+    @board = Board.new
+    @grid = @board.grid
+    @node = @grid[3][3]
   end
 
   it 'exists' do
-    board = Board.new
-    grid = board.grid
-    node = grid[0][0]
-    expect(node).to be_a(Node)
+    expect(@node).to be_a(Node)
   end
 
   it 'has attributes' do
-    board = Board.new
-    grid = board.grid
-    node = grid[0][0]
-    expect(node).to be_a(Node)
-    expect(node.letter).to eq('.')
-    require 'pry'; binding.pry
-    node.link_up
-    expect(node.ne).to eq(grid[1][1])
-    expect(node.e).to eq(grid[1][0])
-    expect(node.se).to eq(nil)
-    expect(node.s).to eq(nil)
-    expect(node.sw).to eq(nil)
-    expect(node.w).to eq(nil)
-    expect(node.nw).to eq(nil)
-    expect(node.empty?).to eq(true)
+    expect(@node).to be_a(Node)
+    expect(@node.letter).to eq('.')
+    expect(@node.ne).to eq(@grid[4][2])
+    expect(@node.e).to eq(@grid[4][3])
+    expect(@node.se).to eq(@grid[4][4])
+    expect(@node.s).to eq(@grid[3][4])
+    expect(@node.sw).to eq(@grid[2][4])
+    expect(@node.w).to eq(@grid[2][3])
+    expect(@node.nw).to eq(@grid[2][2])
   end
 
-  # it 'can count' do
-  #   board = Board.new
-  #   grid = board.grid
-  #   board.place('X', 0)
-  #   expect(grid[0][0].letter).to eq('X')
-  #   expect(grid[0][0].connect).to eq(1)
-  # end
+  it 'can count' do
+    # require 'pry'; binding.pry
+    expect(@node.count("se")).to eq(2)
+    expect(@node.count("se", 'x')).to eq(0)
+    expect(@node.connect('x')).to eq(1)
+    expect(@node.connect('.') > 4)
+    expect(@node.connect?).to eq(true)
+  end
 end
