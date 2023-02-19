151c07ee563a2e50d1c98bd5f7c980e2184e1e20
Andrew Bowman <andrewtbowman@gmail.com>
2023-02-19 15:49:06 -0500

    refactor: simplify prompt loops

--- lib/game.rb
+++ lib/game.rb
@@ -1,16 +1,11 @@
-require './spec/spec_helper'
-
 class Game
-  attr_reader :board, :game, :grid, :pick
-  attr_accessor :under
+  attr_reader :board, :grid, :under, :result
 
   def initialize
-    @game = self
-    @under = true
     @board = Board.new(self)
     @grid = @board.grid
+    @under = false
     @result = ''
-    @pick = -1
   end
 
   def start
@@ -27,7 +22,6 @@ class Game
     if answer.downcase == 'p'
       puts "\e[H\e[2J"
       puts "Let the games begin!"
-      answer = ''
       play
     elsif answer.downcase == 'q'
       puts "\e[H\e[2J"
@@ -38,58 +32,57 @@ class Game
   end
 
   def play
+    @board.clear
     @under = true
-    until @under == false
+    until !@under
       @board.render
-      prompt 
-      @board.places('X', @pick)
+      @board.place("X", prompt)
       @board.render("\nThe computer will now play a most cunning move.")
       sleep(2)
-      @board.places("O")
+      @board.place("O")
     end
   end
 
   def prompt
     puts "Place your X!"
     letter = gets.chomp
-    @pick = -1
-    until @pick.between?(0,6)
-      if letter.downcase    == 'a' && @grid[0][5].empty?
-        @pick = 0
-      elsif letter.downcase == 'b' && @grid[1][5].empty?
-        @pick = 1
-      elsif letter.downcase == 'c' && @grid[2][5].empty?
-        @pick = 2
-      elsif letter.downcase == 'd' && @grid[3][5].empty?
-        @pick = 3
-      elsif letter.downcase == 'e' && @grid[4][5].empty?
-        @pick = 4
-      elsif letter.downcase == 'f' && @grid[5][5].empty?
-        @pick = 5
-      elsif letter.downcase == 'g' && @grid[6][5].empty?
-        @pick = 6
-      else
-        puts "\e[H\e[2J"
-        @board.render("\nInvalid input, try again.")
-        prompt
-      end
+
+    if letter.downcase    == 'a' && @grid[0][5].empty?
+       return 0
+    elsif letter.downcase == 'b' && @grid[1][5].empty?
+       return 1
+    elsif letter.downcase == 'c' && @grid[2][5].empty?
+       return 2
+    elsif letter.downcase == 'd' && @grid[3][5].empty?
+       return 3
+    elsif letter.downcase == 'e' && @grid[4][5].empty?
+       return 4
+    elsif letter.downcase == 'f' && @grid[5][5].empty?
+       return 5
+    elsif letter.downcase == 'g' && @grid[6][5].empty?
+       return 6
+    else
+      puts "\e[H\e[2J"
+      @board.render("\nInvalid input, try again.")
+      prompt
     end
   end
 
-  def over
+  def over(winner)
     @under = false
+    @result = winner
     @board.render("\nOh snap! Good game!")
     sleep(3)
-    @board.clear
     playorquit
   end
-    # if result == 'X'
-    #   puts "You Win!"
-    # elsif result == 'O'
-    #   puts "You lose!"
-    # elsif result == 'draw' 
-    #   puts "Tie game!"
-    # else
-    #  puts "error: invalid end of game"
-    # end
 end
+
+# if result == 'X'
+#   puts "You Win!"
+# elsif result == 'O'
+#   puts "You lose!"
+# elsif result == 'draw' 
+#   puts "Tie game!"
+# else
+#  puts "error: invalid end of game"
+# end
