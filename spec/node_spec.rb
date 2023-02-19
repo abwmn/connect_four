e85b0ad528f4257feee54cb391c64cd404748a71
Andrew Bowman <andrewtbowman@gmail.com>
2023-02-18 22:46:30 -0500

    update count tests

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -14,21 +14,28 @@ RSpec.describe do
   it 'has attributes' do
     expect(@node).to be_a(Node)
     expect(@node.letter).to eq('.')
-    expect(@node.ne).to eq(@grid[4][2])
+    @node.letter = 'X'
+    expect(@node.ne).to eq(@grid[4][4])
+    @node.ne.letter = 'O'
+    @board.render
     expect(@node.e).to eq(@grid[4][3])
-    expect(@node.se).to eq(@grid[4][4])
-    expect(@node.s).to eq(@grid[3][4])
-    expect(@node.sw).to eq(@grid[2][4])
+    expect(@node.se).to eq(@grid[4][2])
+    expect(@node.s).to eq(@grid[3][2])
+    expect(@node.sw).to eq(@grid[2][2])
     expect(@node.w).to eq(@grid[2][3])
-    expect(@node.nw).to eq(@grid[2][2])
+    expect(@node.nw).to eq(@grid[2][4])
   end
 
   it 'can count' do
     # require 'pry'; binding.pry
-    expect(@node.count("se")).to eq(2)
+    expect(@node.count("se")).to eq(3)
     expect(@node.count("se", 'x')).to eq(0)
-    expect(@node.connect('x')).to eq(1)
+    @node.se.letter='X'
+    @board.render
+    expect(@node.count('se', 'X')).to eq(1)
+    expect(@node.connect('X')).to eq(2)
     expect(@node.connect('.') > 4)
     expect(@node.connect?).to eq(true)
+    expect(@node.connect?(4, 'O')).to eq(false)
   end
 end
