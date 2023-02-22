7e2f16c314c7db93203b2b10e5df2d91a998d3b1
Logan Cole <emtlogancole@gmail.com>
2023-02-22 15:15:06 -0600

    test: computer class

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -1,9 +1,15 @@
+require_relative 'picker'
+require_relative 'player'
 class Computer < Player
-  attr_reader :difficulty, :letter
-  def initialize
+  include Picker
+  attr_accessor :difficulty
+  attr_reader  :letter, :turn
+  def initialize #maybe use initialize to set difficulty or turn or something, lol
     @difficulty = ''
-    
+    @turn = 'c'
   end
 
-  
+  def change_letter(new_letter)
+    super
+  end
 end
