b32d736ab6883171be4ccb2112fd195ed70c1050
Logan Cole <emtlogancole@gmail.com>
2023-02-22 15:13:13 -0600

    test: player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,10 @@
 class Player
+  attr_reader :letter
   def initialize
     @letter = ''
+  end
+  
+  def change_letter(new_letter)
+    @letter = "#{new_letter}"
   end
 end
---
+++ spec/player_spec.rb
@@ -0,0 +1,20 @@
+require 'rspec'
+require './lib/player'
+
+RSpec.describe Player do
+  before(:each) do
+    @player = Player.new
+  end
+  it 'exists' do
+    expect(@player).to be_a(Player)
+  end
+
+  it 'has letter' do
+    expect(@player.letter).to eq('')
+  end
+
+  it 'changes letter' do
+    @player.change_letter('O')
+    expect(@player.letter).to eq('O')
+  end
+end
