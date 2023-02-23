5f758f4938b0d996c1ed3369947959499683a34d
Andrew Bowman <andrewtbowman@gmail.com>
2023-02-23 02:30:47 -0500

    refactor: DRY up picker

--- lib/picker.rb
+++ lib/picker.rb
@@ -1,15 +1,21 @@
 require_relative 'requirements'
 module Picker
   def pick(player)
+    case player.letter
+    when 'X'
+      other_letter = 'O'
+    when 'O'
+      other_letter = 'X'
+    end
     case player.difficulty
     when 'e'
       easy_pick
     when 'm'
-      medium_pick(player)
+      medium_pick(player.letter, other_letter)
     when 'h'
-      hard_pick(player)
+      hard_pick(player.letter, other_letter)
     when 'i'
-      insane_pick(player)
+      insane_pick(player.letter, other_letter)
     end
   end
   
@@ -23,17 +29,11 @@ module Picker
     col
   end
 
-  def medium_pick(player)
-    case player.letter
-    when 'X'
-      other_letter = 'O'
-    when 'O'
-      other_letter = 'X'
-    end
+  def medium_pick(letter, other_letter)
     movescores = {}
     (0..6).each do |col|
       node = @grid[col].find { |node| node.empty?}
-      if node && node.connect?(4, player.letter)
+      if node && node.connect?(4, letter)
         return col
       elsif node && node.connect?(4, other_letter)
         movescores[col] = 2
@@ -48,29 +48,23 @@ module Picker
     end.sample 
   end
 
-  def hard_pick(player)
-    case player.letter
-    when 'X'
-      other_letter = 'O'
-    when 'O'
-      other_letter = 'X'
-    end
+  def hard_pick(letter, other_letter)
     return rand(0..6) if @moves < 4
     movescores = {}
     (0..6).each do |col|
       node = @grid[col].find { |node| node.empty?}
-      if node && node.connect?(4, player.letter)
+      if node && node.connect?(4, letter)
         return col
       elsif node && node.connect?(4, other_letter)
         movescores[col] = 4
-      elsif node && node.connect?(3, player.letter)
-        if !node.n && node.count('s', player.letter) == 2
+      elsif node && node.connect?(3, letter)
+        if !node.n && node.count('s', letter) == 2
           movescores[col] = 1
         else  
           movescores[col] = 3
         end
       elsif node
-        movescores[col] = node.connect(player.letter)
+        movescores[col] = node.connect(letter)
       else
         movescores[col] = 0
       end
@@ -80,36 +74,30 @@ module Picker
     end.sample 
   end
 
-  def insane_pick(player)
-    case player.letter
-    when 'X'
-      other_letter = 'O'
-    when 'O'
-      other_letter = 'X'
-    end
+  def insane_pick(letter, other_letter)
     return rand(0..6) if @moves < 2
     movescores = {}
     (0..6).each do |col|
       node = @grid[col].find { |node| node.empty?}
-      if node && node.connect?(4, player.letter)
+      if node && node.connect?(4, letter)
         return col
       elsif node && node.connect?(4, other_letter)
-        if node.any_traps?(player.letter)
+        if node.any_traps?(letter)
           movescores[col] = 6
         else
           movescores[col] = 5
         end
-      elsif node && node.connect?(3, player.letter)
-        if !node.n && node.count('s', player.letter) == 2
+      elsif node && node.connect?(3, letter)
+        if !node.n && node.count('s', letter) == 2
           movescores[col] = 1
         else
           movescores[col] = 3
         end
-        if node.any_traps?(player.letter)
+        if node.any_traps?(letter)
           movescores[col] = 4
         end
       elsif node
-        movescores[col] = node.connect(player.letter)
+        movescores[col] = node.connect(letter)
       else
         movescores[col] = 0
       end
