a88cedea8f49c8658a452b792cf81e235f6c18a1
Logan Cole <emtlogancole@gmail.com>
2023-02-23 12:40:39 -0600

    test: initialize

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -8,18 +8,12 @@ RSpec.describe do
     expect(@game).to be_a(Game)
   end
 
-  it 'has attributes' do
-    expect(@game.board).to be_a(Board)
-    # expect(@game.grid).to eq(@board.grid)
-    expect(@game.under).to eq(true)
-    expect(@game.winner).to eq(false)
-    expect(@game.lastwinner).to eq(false)
-    # expect(@game.message).to eq('')
-    expect(@game.moves).to eq(0)
-    expect(@game.player_wins).to eq(0)
-    expect(@game.foe_wins).to eq(0)
-    expect(@game.draws).to eq(0)
-    expect(@game.player1).to eq(false)
-    expect(@game.player2).to eq(false)
+  it 'initializes players' do
+    expect(@game.player1.letter).to eq('X')
+    expect(@game.player2.letter).to eq('O')
+  end
+
+  it 'initializes board' do
+    @game.board.render
   end
 end
